08eeec1ef91851193f16bee8245fb42c9ab76d85
Taylor Mock <tmock12@gmail.com>
2018-07-23 17:08:57 +0300

    Add basic UI for marketing calendar

--- lib/gymotion_web/controllers/marketing_controller.ex
+++ lib/gymotion_web/controllers/marketing_controller.ex
@@ -2,6 +2,10 @@ defmodule GymotionWeb.MarketingController do
   use GymotionWeb, :controller
 
   def index(conn, _params) do
-    render conn, "index.html"
+    classes = [
+      %{ name: "Pilates" },
+      %{ name: "Abs & Butts" },
+    ]
+    render conn, "index.html", classes: classes
   end
 end

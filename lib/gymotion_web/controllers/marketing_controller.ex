d73bd1b322e088b048172b903b31dabb95675312
Taylor Mock <tmock12@gmail.com>
2018-07-30 19:40:36 +0300

    fixup marketing

--- lib/gymotion_web/controllers/marketing_controller.ex
+++ lib/gymotion_web/controllers/marketing_controller.ex
@@ -1,11 +1,13 @@
 defmodule GymotionWeb.MarketingController do
   use GymotionWeb, :controller
+  plug(:put_layout, "marketing.html")
 
   def index(conn, _params) do
     classes = [
-      %{ name: "Pilates" },
-      %{ name: "Abs & Butts" },
+      %{name: "Pilates"},
+      %{name: "Abs & Butts"}
     ]
-    render conn, "index.html", classes: classes
+
+    render(conn, "index.html", classes: classes)
   end
 end

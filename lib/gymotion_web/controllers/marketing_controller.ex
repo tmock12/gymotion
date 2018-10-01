66f0e7226e2817850f9edd79a6121fac4a911e4a
Taylor Mock <tmock12@gmail.com>
2018-10-01 17:54:14 +0300

    Better card UI for schedule

--- lib/gymotion_web/controllers/marketing_controller.ex
+++ lib/gymotion_web/controllers/marketing_controller.ex
@@ -4,8 +4,49 @@ defmodule GymotionWeb.MarketingController do
 
   def index(conn, _params) do
     classes = [
-      %{name: "Pilates"},
-      %{name: "Abs & Butts"}
+      %{
+        day: "Luni",
+        classes: [
+          %{name: "Zumba Fitness", time: "19:00"},
+          %{name: "Barre Stretch", time: "20:00"}
+        ]
+      },
+      %{
+        day: "Marţi",
+        classes: [
+          %{name: "Pilates", time: "7:00"},
+          %{name: "Pilates", time: "20:00"}
+        ]
+      },
+      %{
+        day: "Miercuri",
+        classes: [
+          %{name: "Pilates", time: "7:00"},
+          %{name: "Tone it up 30'", time: "19:00"},
+          %{name: "Zumba Fitness", time: "20:00"}
+        ]
+      },
+      %{
+        day: "Joi",
+        classes: [
+          %{name: "Tone it up 30'", time: "18:00"},
+          %{name: "Zumba Fitness", time: "19:00"},
+          %{name: "Barre Stretch", time: "20:00"}
+        ]
+      },
+      %{
+        day: "Vineri",
+        classes: [
+          %{name: "Pilates", time: "18:00"},
+          %{name: "Zumba Fitness", time: "19:00"}
+        ]
+      },
+      %{
+        day: "Sâmbătă",
+        classes: [
+          %{name: "Barre Stretch", time: "10:00"}
+        ]
+      }
     ]
 
     render(conn, "index.html", classes: classes)

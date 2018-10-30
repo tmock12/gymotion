c35521e179e969557cf2a38bed812d9e8d645dec
Taylor Mock <tmock12@gmail.com>
2018-10-30 09:46:49 +0200

    Update classes for current week

--- lib/gymotion_web/controllers/marketing_controller.ex
+++ lib/gymotion_web/controllers/marketing_controller.ex
@@ -7,47 +7,45 @@ defmodule GymotionWeb.MarketingController do
       %{
         day: "Luni",
         classes: [
-          %{name: "Barre Stretch", time: "18:00"},
           %{name: "Toning", time: "19:00"},
-          %{name: "Zumba Fitness", time: "20:00"}
+          %{name: "Pilates", time: "20:00"}
         ]
       },
       %{
         day: "Marţi",
         classes: [
           %{name: "Pilates", time: "7:00"},
-          %{name: "Barre Fitness", time: "19:00"},
+          %{name: "Tabata", time: "19:00"},
           %{name: "Pilates", time: "20:00"}
         ]
       },
       %{
         day: "Miercuri",
         classes: [
-          %{name: "Barre Fitness", time: "18:00"},
           %{name: "Zumba Fitness", time: "19:00"},
-          %{name: "Pilates", time: "20:00"}
+          %{name: "Barre Fitness", time: "20:00"}
         ]
       },
       %{
         day: "Joi",
         classes: [
           %{name: "Pilates", time: "7:00"},
-          %{name: "Trim & Tone", time: "18:00"},
-          %{name: "Barre Fitness", time: "19:00"},
-          %{name: "Zumba Fitness", time: "20:00"}
+          %{name: "Trim & Tone", time: "18:20"},
+          %{name: "Zumba Fitness", time: "19:00"},
+          %{name: "Barre Fitness", time: "20:00"}
         ]
       },
       %{
         day: "Vineri",
         classes: [
           %{name: "Pilates", time: "18:00"},
-          %{name: "Tabata", time: "19:00"}
+          %{name: "Barre Fitness", time: "19:00"}
         ]
       },
       %{
         day: "Sâmbătă",
         classes: [
-          %{name: "Pilates", time: "10:00"}
+          %{name: "Barre Stretch", time: "10:00"}
         ]
       }
     ]
